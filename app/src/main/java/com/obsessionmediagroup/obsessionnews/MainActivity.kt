6f14b32157b371ff67d714d9154c1825b2ffb9a3
Dimas Putra Widiyanto <putrad322@gmail.com>
2023-01-31 14:05:53 +0700

    Latest update

--- app/src/main/java/com/obsessionmediagroup/obsessionnews/MainActivity.kt
+++ app/src/main/java/com/obsessionmediagroup/obsessionnews/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.obsessionmediagroup.obsessionnews
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.core.view.WindowInsetsCompat
 
+@SuppressLint("SetJavaScriptEnabled")
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        WindowInsetsCompat.Type.navigationBars()
+
+        webView = findViewById(R.id.webView)
+
+        // WebViewClient allows you to handle
+        // onPageFinished and override Url loading.
+        webView.webViewClient = WebViewClient()
+
+        // this will load the url of the website
+        webView.loadUrl(WEB_URL)
+
+        // this will enable the javascript settings, it can also allow xss vulnerabilities
+        webView.settings.javaScriptEnabled = true
+
+        // if you want to enable zoom feature
+        webView.settings.setSupportZoom(true)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    companion object {
+        private const val WEB_URL = "https://www.obsessionnews.com/"
     }
 }
